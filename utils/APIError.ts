f7701dfc0d5d1dd395f89cdcd389dfde61a8f141
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-07 20:27:18 +0530

    refactor : (utils/APIError.ts) from require to import for pure ESM

--- utils/APIError.ts
+++ utils/APIError.ts
@@ -1,7 +1,10 @@
 class APIError extends Error {
   statusCode: Number;
-  constructor(message: string, statusCode = 500) {
+  constructor(message: string, statusCode: number= 500) {
     super(message);
     this.statusCode = statusCode;
+      this.name = "API Error"; 
   }
 }
+
+export default APIError;
