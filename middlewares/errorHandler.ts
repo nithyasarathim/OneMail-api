a5fb11052d6c50678c7bf6f10803d82b0ea1a768
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-07 21:05:17 +0530

    chore : (middlewares/requestLogger.ts) Middleware added to log client requests and status

--- middlewares/errorHandler.ts
+++ middlewares/errorHandler.ts
@@ -2,14 +2,14 @@ import type { Request, Response, NextFunction } from "express";
 import APIError from '../utils/APIError.js';
 
 const errorHandler = function (
-  err: any,
+  err: unknown,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  console.error(err.stack || err);
+  console.error(err);
   if (err instanceof APIError) {
-    return res.status(err.statusCode).json({
+    return res.status(Number(err.statusCode)).json({
       success: false,
       message: err.message,
     });
