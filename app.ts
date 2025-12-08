cdea217b613d6f47e4fb7f2f978da40f2361f907
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-08 11:47:27 +0530

    feat: added root health-check endpoint for server status

--- app.ts
+++ app.ts
@@ -4,11 +4,22 @@ import requestLogger from "./middlewares/requestLogger.js";
 import signatureValidator from "./middlewares/signatureValidator.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import type { Request,Response } from "express";
 
 const app = express();
 
 app.use(express.json());
 app.use(requestLogger);
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        success: true,
+        message:"Auth Mail server is alive on this port"
+    })
+})
+ 
+
 app.use('/otp', rateLimiter ,signatureValidator, OTPRouter);
 app.use(errorHandler);
 
